cfc6cb24f84d2d284c4dcd7bc3c7364c927d0b6d
JASSIM-AL-HUMAID <152396941+JASSIM-AL-HUMAID@users.noreply.github.com>
2024-09-16 15:09:04 +0300

    java code

---
+++ test.java
@@ -0,0 +1,14 @@
+public class test{
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("first DEV");
+        int i =0 ;
+        while(i <= 100){
+            if (i == 100)
+                System.out.println(i) ;
+            else 
+                System.out.print(i+" , ");
+            i += 10 ; 
+        } 
+    }
+}
